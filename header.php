53cd0a58fe81ef19c50f8999bb1fd2dc8def56f8
Inge Fossland <inge@fossland.com>
2012-12-02 21:55:24 +0100

    fix links [deploy: production]

--- header.php
+++ header.php
@@ -42,7 +42,7 @@
 		<h2>Meny</h2>
 
 		<ul>
-			<li><a href="<?php bloginfo('url'); ?>/season/">Sesonger</a></li>
+			<li><a href="<?php bloginfo('url'); ?>/matches/">Sesonger</a></li>
 			<li><a href="<?php bloginfo('url'); ?>/players/">Spillere</a></li>
 			<li><a href="<?php bloginfo('url'); ?>/teams/">Motstandere</a></li>
 			<li><a href="<?php bloginfo('url'); ?>/grounds/">Baner</a></li>
