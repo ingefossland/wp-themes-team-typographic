45fba471f5ec59aeecc60d4113d150ab626684fd
Inge Fossland <inge@fossland.com>
2012-12-05 10:24:40 +0100

    fix [deploy: production]

--- js/team-iOS.js
+++ js/team-iOS.js
@@ -3,6 +3,7 @@ jQuery(document).ready(function($) {
 	function iOSswipeLeft() {
 //		alert("Swipe left");
 
+		// hide menu if visible, else open filter
         if ($('#menu').is(':visible')) {
             hide_menu();
         } else {
@@ -14,6 +15,7 @@ jQuery(document).ready(function($) {
 	function iOSswipeRight() {
 //		alert("Swipe right");
 
+		// hide filter if visible, else open menu
         if ($('#filter').is(':visible')) {
             hide_filter();
         } else {
